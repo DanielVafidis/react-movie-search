522e2cffb9d230897134feeb1dd0357356338529
DanielVafidis <daniel.vafidis@gmail.com>
2019-11-15 00:43:30 +0100

    Updates

---
+++ precache-manifest.d95834571f582f25a2e0f3a76973a6a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0869a3d86f82176dfba6dff67cf985f",
+    "url": "/react-movie-search/index.html"
+  },
+  {
+    "revision": "c204e577ab3af5015149",
+    "url": "/react-movie-search/static/css/main.739aba6a.chunk.css"
+  },
+  {
+    "revision": "91dd79389a0fcb74e997",
+    "url": "/react-movie-search/static/js/2.0379e1e2.chunk.js"
+  },
+  {
+    "revision": "c204e577ab3af5015149",
+    "url": "/react-movie-search/static/js/main.63ebc853.chunk.js"
+  },
+  {
+    "revision": "69c11d04f8afd4c89cb4",
+    "url": "/react-movie-search/static/js/runtime-main.8beab208.js"
+  }
+]);
